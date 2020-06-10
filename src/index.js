8b784ed95780e33b4a1dd266d46a0be2c0ca1231
Ammar Halees <haleesammar@gmail.com>
2020-06-10 14:19:44 +0300

    Fixing routes for netlify ci/cd

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/pinkrevoltdemos.netlify.app">
+    <BrowserRouter>
         <App />
 
     
