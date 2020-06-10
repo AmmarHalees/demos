884db8fa1b1250c1234e5b5677a6ee5a28a0584b
Ammar Halees <haleesammar@gmail.com>
2020-06-10 14:43:33 +0300

    test removing dynamically generated routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import RouteWithSubRoutes from './components/RouteWithSubRoutes/RouteWithSubRoutes';
-import { routes_data } from './utils/routesconfig';
 
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './containers/Home/Home';
+import Projects from './containers/Projects/Projects';
+import Bus from './containers/Bus/Bus';
+import ThankYou from './containers/ThankYou/ThankYou';
+import Main from './containers/Projects/Main/Main';
+import NotFound from './containers/NotFound/NotFound';
 
+const routes_data = [
+  {
+    path: ['/home', '/', '/ammar'],
+    component: Home,
+    exact: true
 
+  },
+  {
+    path: "/projects",
+    component: Projects,
+    routes: [
+
+      {
+        path: "/projects/bus",
+        component: Bus,
+
+      }
+      ,
+      {
+        path: "/projects/thankyou",
+        component: ThankYou,
+
+      }
+      ,
+
+      {
+        path: "/projects",
+        component: Main,
+        // exact:true
+
+      }
+
+    ]
+  }
+  ,
+  {
+    component: NotFound,
+  }
+];
 
 function App() {
   return (
@@ -19,15 +61,15 @@ function App() {
       <div style={{minHeight:'600px'}}>
 
         <Switch>
-
+{/* 
           <Route  path="/">
 
                 <Home/>
 
-          </Route>
-          {/* {routes_data.map((route, i) => (
+          </Route> */}
+          {routes_data.map((route, i) => (
             <RouteWithSubRoutes key={i} {...route} />
-          ))} */}
+          ))}
         </Switch>
 
       </div>
