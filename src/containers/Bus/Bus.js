3c1b29ada6fe14e65ea5f86a52957654507c956b
Ammar Halees <haleesammar@gmail.com>
2020-06-10 15:43:54 +0300

    added button links to products

--- src/containers/Bus/Bus.js
+++ src/containers/Bus/Bus.js
@@ -5,14 +5,25 @@ import Iframe from 'react-iframe';
 const Bus = () => {
     return (
 
+        <div className='project_item_container'>
 
             <Iframe url="/DigitalBusinessCard/index.html"
                 id="myId"
                 className="iframe"
             />
 
+            
+<a class="_button_basic _primary_button" href="https://www.etsy.com/listing/804777610/limited-digital-business-card-template"
+                role="button">
+
+                Buy it Here
+            </a>
+
+
+        </div>
 
     );
 }
 
 export default Bus;
+
--- src/containers/ThankYou/ThankYou.js
+++ src/containers/ThankYou/ThankYou.js
@@ -3,10 +3,21 @@ import Iframe from 'react-iframe';
 
 const ThankYou = () => {
     return (
-        <Iframe url="/ThankYou/index.html"
-            id="myId"
-            className="iframe"
-        />
+        <div className='project_item_container'>
+
+
+            <a class="_button_basic _primary_button" href="https://www.etsy.com/listing/804253086/limited-zebra-stripes-interactive"
+                role="button">
+
+                    Buy it Here
+            </a>
+
+            <Iframe url="/ThankYou/index.html"
+                id="myId"
+                className="iframe"
+            />
+
+        </div>
 
     );
 }
